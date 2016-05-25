29f0fdfdf21ebfd3840c744565a72e191d675a5c
nikhilbiyani <nikhilbiyani@gmail.com>
2016-05-25 13:40:18 +0200

    Initialized fouirer filter

--- src/algorithm/fourier_filter.hpp
+++ src/algorithm/fourier_filter.hpp
@@ -19,11 +19,77 @@
 #ifndef FOURIER_FILTER_HPP
 #define FOURIER_FILTER_HPP
 
+#include <type_traits>
+
 namespace em {
     
     namespace algorithm {
         
-    
+        enum class FilterAlgorithm {
+            TOP_HAT,
+            GAUSSIAN,
+            BUTTERWORTH
+        };
+        
+        template<typename ObjectType_, FilterAlgorithm algo_>
+        struct FilteringImplementaiton {
+            
+        };
+        
+        template<typename ObjectType_>
+        struct FilteringImplementation<ObjectType_, FilterAlgorithm::TOP_HAT> {
+            
+            void low_pass(ObjectType_& obj, double cut_off_freq) {
+                for(auto& data : obj) {
+                    if(resolution(data.index(), obj.cell_lengths(), obj.cell_angle()) >= cut_off_freq) {
+                        obj[data.index()] = obj::object_traits<ObjectType_>::data_type();
+                    }
+                }
+            }
+            
+            void high_pass(ObjectType_& obj, double cut_off_freq) {
+                for(auto& data : obj) {
+                    if(resolution(data.index(), obj.cell_lengths(), obj.cell_angle()) <= cut_off_freq) {
+                        obj[data.index()] = obj::object_traits<ObjectType_>::data_type();
+                    }
+                }
+            }
+            
+            void band_pass(ObjectType_& obj, double low_pass_freq, double high_pass_freq) {
+                for(auto& data : obj) {
+                    if(resolution(data.index(), obj.cell_lengths(), obj.cell_angle()) >= low_pass_freq || 
+                       resolution(data.index(), obj.cell_lengths(), obj.cell_angle()) <= high_pass_freq) {
+                        obj[data.index()] = obj::object_traits<ObjectType_>::data_type();
+                    }
+                }
+            }
+            
+        };
+        
+        template<typename ObjectType_>
+        struct FilteringImplementation<ObjectType_, FilterAlgorithm::BUTTERWORTH> {
+            
+            //TODO
+            
+        };
+        
+        template<typename ObjectType_>
+        struct FilteringImplementation<ObjectType_, FilterAlgorithm::GAUSSIAN> {
+            
+            //TODO
+            
+        };
+        
+        
+        
+        
+        template<typename ObjectType_>
+        std::enable_if<object::is_complex_valued<typename object::object_traits<ObjectType_>::data_type>::value, void>::type
+        low_pass(ObjectType_& object) {
+            
+        }
+        
+        
         
     }
 }
---
+++ src/algorithm/resolution_calculator.hpp
@@ -0,0 +1,73 @@
+/* 
+ * This file is a part of emkit.
+ * 
+ * emkit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any 
+ * later version.
+ * 
+ * emkit is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public 
+ * License for more details <http://www.gnu.org/licenses/>
+ * 
+ * Author:
+ * Nikhil Biyani: nikhil(dot)biyani(at)gmail(dot)com
+ * 
+ */
+
+#ifndef RESOLUTION_CALCULATOR_HPP
+#define RESOLUTION_CALCULATOR_HPP
+
+#include <iostream>
+#include <array>
+#include <cmath>
+
+#include "../elements/index.hpp"
+
+namespace em {
+
+    namespace algorithm {
+
+        template<size_t rank_>
+        double resolution(Index<rank_> index, std::array<double, rank_> cell_lengths, double cell_angle) {
+        
+            for(int i=0; i<rank_; ++i) {
+                if (cell_lengths[i] == 0 || cell_angle == 0) {
+                    std::cerr << "ERROR! Encountered 0 in cell dimensions while calculating resolution.";
+                    return 0;
+                }
+            }
+                        
+            std::array<double, rank_> rec_lengths;
+            for(int i=0; i<rank_-1; ++i) {
+                rec_lengths[i] = 1.0/(cell_lengths[i] * sin(cell_angle));
+            }
+            rec_lengths[rank_-1] = 1.0/cell_lengths[rank_-1];
+            
+            double recgamma = M_PI - gamma;
+            double dstar_sq_sum = 0;
+            double index_rec_prod = 1;
+            for(int i=0; i< rank_; ++i) {
+                dstar_sq_sum += pow(index[i]*rec_lengths[i], 2);
+                if(i < rank_-1) index_rec_prod = index[i]*rec_lengths[i];
+            }
+            dstar_sq_sum + 2*index_rec_prod*cos(recgamma);
+            
+            return sqrt(dstar_sq_sum);
+            
+        }
+        
+        template<size_t rank_>
+        double resolution(Index<rank_> index, std::array<double, rank_> cell_lengths) {
+            
+            resolution(index, cell_lengths, M_PI/2);
+
+        }
+
+
+    }
+}
+
+#endif
+
